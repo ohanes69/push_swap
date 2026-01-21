970402495e544bcd254d7fd75be30b004f9e4a16
Sacha Markarian <samarkar@z1r12p1.42lyon.fr>
2026-01-21 17:13:28 +0100

    new disorder

--- V2_loading/compute_disorder.c
+++ V2_loading/compute_disorder.c
@@ -6,26 +6,35 @@
 /*   By: samarkar <samarkar@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 17:55:26 by samarkar          #+#    #+#             */
-/*   Updated: 2026/01/20 18:38:41 by samarkar         ###   ########lyon.fr   */
+/*   Updated: 2026/01/21 16:50:20 by samarkar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "push_swap.h"
 
 #include "push_swap.h"
 
 float	compute_disorder(t_tab *a)
 {
 	size_t	i;
+	size_t 	j;
 	size_t	total_pairs;
 	size_t	mistakes;
 	float	metric;
 
-	total_pairs = a->size - 1;
+	total_pairs = 0;
 	mistakes = 0;
 	i = 0;
-	while (i < total_pairs)
+	while (i < a->size - 1)
 	{
-		if (a->tab[i] > a->tab[i + 1])
-			mistakes++;
+		j = i + 1;
+		while (j < a->size - 1)
+		{
+			total_pairs += 1;
+			if (a->tab[i] > a->tab[j])
+				mistakes++;
+			j++;
+		}
 		i++;
 	}
 	metric = ((float)mistakes / total_pairs);
