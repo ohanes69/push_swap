98fc5ceeca8a6eee7a0ed697b155167bab1bfff4
Sacha Markarian <samarkar@z3r13p1.42lyon.fr>
2026-01-22 14:53:29 +0100

    q

---
+++ for_lucas/main2.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main2.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samarkar <samarkar@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 11:02:56 by samarkar          #+#    #+#             */
+/*   Updated: 2026/01/22 12:51:46 by samarkar         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap2.h"
+#include <stdio.h>
+
+void	init_stack(t_tab *stack)
+{
+	stack->simple = 0;
+	stack->medium = 0;
+	stack->complex = 0;
+	stack->adaptative = 0;
+	stack->bench = 0;
+	stack->sa = 0;
+	stack->sb = 0;
+	stack->ss = 0;
+	stack->pa = 0;
+	stack->pb = 0;
+	stack->ra = 0;
+	stack->rb = 0;
+	stack->rr = 0;
+	stack->rra = 0;
+	stack->rrb = 0;
+	stack->rrr = 0;
+}
+
+static void	free_all(char **args, t_tab *a, t_tab *b)
+{
+	ft_free_tab(args);
+	free(a);
+	free(b);
+}
+
+size_t	nb_args(char **args)
+{
+	size_t	i;
+
+	i = 0;
+	while (args[i])
+		i++;
+	return (i);
+}
+
+size_t	find_size(char **args)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (args[i])
+	{
+		if (is_valid_int2(args[i]) == 1)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+int	main(int ac, char **av)
+{
+	t_tab	*a;
+	t_tab	*b;
+	char	**args;
+	size_t	size;
+
+	if (ac < 2)
+		return (0);
+	a = malloc(sizeof(t_tab));
+	if (!a)
+	{
+		free(a);
+		return (1);
+	}
+	b = malloc(sizeof(t_tab));
+	if (!b)
+	{
+		free(a);
+		free(b);
+		return (1);
+	}
+	args = NULL;
+	init_stack(a);
+	args = parsing(ac, av);
+	if (!args)
+	{
+		free_all(args, a, b);
+		return (1);
+	}
+	size = find_size(args);
+	push_swap(args, a, b, size);
+	// free(a->tab);
+	// free(b->tab);
+	// free_all(args, a, b);
+}
