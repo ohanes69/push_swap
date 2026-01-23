14d28d97ca50d499fc8c08968083078f27f03491
Sacha Markarian <samarkar@z3r12p1.42lyon.fr>
2026-01-23 19:57:53 +0100

    V4

---
+++ V4/push_swap_utils.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samarkar <samarkar@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 20:53:58 by lucpelle          #+#    #+#             */
+/*   Updated: 2026/01/23 13:35:37 by samarkar         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (!dest && !src)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+
+size_t	ft_strcmp(char *s1)
+{
+	size_t	i;
+	size_t	j;
+	char	*comp[5];
+
+	comp[0] = "--simple";
+	comp[1] = "--medium";
+	comp[2] = "--complex";
+	comp[3] = "--adaptive";
+	comp[4] = "--bench";
+	j = 0;
+	while (j < 5)
+	{
+		i = 0;
+		while ((s1[i] && comp[j][i]) && s1[i] == comp[j][i])
+			i++;
+		if (s1[i] == '\0' && comp[j][i] == '\0')
+			return (j + 1);
+		++j;
+	}
+	return (0);
+}
